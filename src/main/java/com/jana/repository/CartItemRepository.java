36c26219a24767804d137c3026bf397bce1612bd
Jana-JR <janarthanan2skr@gmail.com>
2025-01-04 18:52:44 +0530

    cart api done

--- src/main/java/com/jana/repository/CartItemRepository.java
+++ src/main/java/com/jana/repository/CartItemRepository.java
@@ -1,10 +1,8 @@
 package com.jana.repository;
 
 import com.jana.model.CartItem;
-import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Data
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
 }
